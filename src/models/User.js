03c6f66ebff2fa451b5216a942d788823cd1d049
TeksMarques <taotaodoce@gmail.com>
2023-01-26 16:05:52 -0300

    requisito 2 concluído

---
+++ src/models/User.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+  const users = sequelize.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+    },
+    display_name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    timestamps: false,
+    tableName: 'users',
+  });
+  users.associate = function(models) {
+    users.hasMany(models.BlogPost, { foreignKey: 'user_id', as: 'posts' });
+  };
+  return users;
+}
